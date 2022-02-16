54e70a692d5a8e2cd51cf6794a8144fa7ed7aa79
markjfannon <41549138+markjfannon@users.noreply.github.com>
2022-02-16 13:57:14 +0000

    Can now generally close windows, made class that inherits from Rectangle called startWindow. Currently having button attribute trouble

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def nameCheck(name):
             return False
     return True
 
-
 def updateButtons(button):
     button.update()
     button.changeColour(pg.mouse.get_pos())
@@ -68,10 +67,14 @@ class Rectangle():
     def update(self, position):
         if position[0] in range (self.cross_x,self.cross_x+50) and position[1] in range (self.cross_y, self.cross_y+50):
             print("Click")
+            initScreen()
 
+class startWindow(Rectangle):
+    def __init__(self,x_pos, y_pos, x_size, y_size, colour, titleFont, mainFont, title, body, screenID):
+        super().__init__(x_pos, y_pos, x_size, y_size, colour, titleFont, mainFont, title, body, screenID)
 
-
-
+    
+          
 #button class
 class Button():
     #initialises button
@@ -90,21 +93,23 @@ class Button():
         self.titleFont = pg.font.SysFont("sans-serif", 50)
         self.mainFont = pg.font.SysFont("sans-serif", 25)
     #keeps button updated
-    def update(self, screenID):
-        screenID.screen_item.blit(self.text, self.text_rect)
+    def update(self):
+        self.screenID.screen_item.blit(self.text, self.text_rect)
 
-    def init_image(self, screenID):
-        screenID.screen_item.blit(self.image, self.rect)
+    def init_image(self):
+        self.screenID.screen_item.blit(self.image, self.rect)
       
 
     #checks for clicks
-    def checkForInput(self, position, screenID):
+    def checkForInput(self, position):
         if position[0] in range (self.rect.left, self.rect.right) and position[1] in range (self.rect.top, self.rect.bottom):
             print(self.message)
+            if self.text_input == "Start!":
+                self.rect1=startWindow(50, 50, 500, 400, "Grey", self.titleFont, self.mainFont, self.title, self.body, self.screenID)
             #made rectangle before class was made - old
             #pg.draw.rect(screenID.screen_item, "Black", pg.Rect(48, 48, 504, 404))
             #pg.draw.rect(screenID.screen_item, "Grey", pg.Rect(50, 50, 500, 400))
-            self.rect=Rectangle(50, 50, 500, 400, "Grey", self.titleFont, self.mainFont, self.title, self.body, screenID)
+            self.rect1=Rectangle(50, 50, 500, 400, "Grey", self.titleFont, self.mainFont, self.title, self.body, self.screenID)
             #making text outside of class - old way
             #screenID.screen_item.blit(self.titleFont.render(self.title, True, "Black"), (210, 75))
             #screenID.screen_item.blit(self.mainFont.render(self.body, True, "Black"), (175, 120))
@@ -139,7 +144,6 @@ class startScreen(main):
         self.screen_item.blit(bigLogo, (300, 300))
         self.title=title_font.render("Discipline Tower Defence",True,"Black")
 
-
 #actually calls startScreen class (about time lol)
 startScreen=startScreen(600,600,3,3,3)
 startScreen.screen_item.blit(bigLogo, (300,300))
@@ -149,9 +153,9 @@ def initScreen():
     startScreen.screen_item.blit(bigLogo, (0,0))
     startScreen.screen_item.blit(startScreen.title,(34,275))
 
-    startScreen.startButton.init_image(startScreen)
-    startScreen.infoButton.init_image(startScreen)
-    startScreen.rulesButton.init_image(startScreen)
+    startScreen.startButton.init_image()
+    startScreen.infoButton.init_image()
+    startScreen.rulesButton.init_image()
 
 initScreen()
 
@@ -162,20 +166,20 @@ while True:
             pg.quit()
             sys.exit()
         if event.type == pg.MOUSEBUTTONDOWN:
-               startScreen.startButton.checkForInput(pg.mouse.get_pos(),startScreen)
-               startScreen.infoButton.checkForInput(pg.mouse.get_pos(),startScreen)
-               startScreen.rulesButton.checkForInput(pg.mouse.get_pos(),startScreen)
-               startScreen.rulesButton.checkForInput.rect.update(pg.mouse.get_pos())
-
-
+               startScreen.startButton.checkForInput(pg.mouse.get_pos())
+               startScreen.infoButton.checkForInput(pg.mouse.get_pos())
+               startScreen.rulesButton.checkForInput(pg.mouse.get_pos())
+               startScreen.startButton.rect1.update(pg.mouse.get_pos())
+               #startScreen.infoButton.rect1.update(pg.mouse.get_pos())
+               #startScreen.rulesButton.rect1.update(pg.mouse.get_pos())
                #updateButtons(button)
 
    # for button in buttonList:
-    startScreen.startButton.update(startScreen)
+    startScreen.startButton.update()
     startScreen.startButton.changeColour(pg.mouse.get_pos())
-    startScreen.infoButton.update(startScreen)
+    startScreen.infoButton.update()
     startScreen.infoButton.changeColour(pg.mouse.get_pos())
-    startScreen.rulesButton.update(startScreen)
+    startScreen.rulesButton.update()
     startScreen.rulesButton.changeColour(pg.mouse.get_pos())
     #updateButtons(button)
     pg.display.update()
