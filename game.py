585ac8efa492c3e2f51419fb902cf9b9f1722b83
markjfannon <41549138+markjfannon@users.noreply.github.com>
2022-03-06 23:40:54 +0000

    Finally fixed collision avoidance, added top bar. Apparently it should have been a subroutine to check for collisions from the beginning! Doh! Probably want to look at projectiles and transferring info between main.py and game.py

--- game.py
+++ game.py
@@ -1,17 +1,38 @@
 import pygame as pg
 import random as r
 
+global bigLogo
+logoIcon = pg.image.load("logo.png")
+logoIcon= pg.transform.scale(logoIcon, (40, 40))
+
 screen = pg.display.set_mode((1000,500))
 taken_coords = ["200:250,300:400","500:800,300:400"]
+points=0
+level=1
+bank=0
+name="Joe"
 
+pg.init()
+
+def hasCollided(taken,rect_x,rect_y):
+    for item in taken:
+        split=item.split(",")
+        x=split[0]
+        y=split[1]
+        x=x.split(":")
+        y=y.split(":")
+        if rect_x  >= int(x[0]) and rect_x <= int(x[1]) and rect_y + 50 >= int(y[0]) and rect_y <= int(y[1]):
+            return True
+        
+        
 
 class Rectangle():
-    def __init__(self, x, y, l, h, screen):
+    def __init__(self, x, y, l, h, screen, colour):
         self.x = x
         self.y = y
         self.l = l
         self.h = h
-        pg.draw.rect(screen, "Blue", pg.Rect(self.x, self.y, self.l, self.h))
+        pg.draw.rect(screen, colour, pg.Rect(self.x, self.y, self.l, self.h))
 
     
 class main:
@@ -20,9 +41,18 @@ class main:
         pg.display.set_caption("Discipline Tower Defence")
         self.screen.fill("Black")
         pg.display.flip()
+    def initialise(self,points,level,bank,name):
+        font=pg.font.SysFont("sans-serif",60)
+        topBar=Rectangle(0,0,1000,40,self.screen,"Grey")
+        self.screen.blit(logoIcon,(0,0))
+        self.screen.blit(font.render(str(points),False,"Black"),(160,2))
+        self.screen.blit(font.render(("Level "+str(level)),False,"Black"),(310,2))
+        self.screen.blit(font.render(("$"+str(bank)),False,"Black"),(600,2))
+        self.screen.blit(font.render(name,False,"Black"),(750,2))
+        
     def generateBlocks(self):
-        rect1=Rectangle(200,300,50,100,self.screen)
-        rect2=Rectangle(500,300,300,100,self.screen)
+        rect1=Rectangle(200,300,50,100,self.screen,"Blue")
+        rect2=Rectangle(500,300,300,100,self.screen,"Blue")
 
 
 class enemySprite(pg.sprite.Sprite):
@@ -36,41 +66,30 @@ class enemySprite(pg.sprite.Sprite):
         self.speed = v
         #screen.blit(self.image, (x,y))
     def moveRandomDown(self,taken):
-        l=len(taken)
-        t=0
-        collided = 1
-        while t < l:
-            split=taken[t].split(",")
-            x=split[0]
-            y=split[1]
-            x=x.split(":")
-            y=y.split(":")
-            #if int(collided+1) == int(1):
-               # break
-            if self.rect.x  >= int(x[0]) and self.rect.x <= int(x[1]) and self.rect.y + 50 >= int(y[0]) and self.rect.y <= int(y[1]):
-                print("Moving around")
-                self.rect.x = self.rect.x + 17
-                t=t+10000000000
-            else:
-                print("Moving Down")
-                self.rect.y = self.rect.y + r.randint(0,10)
-            t=t+1
+        collided=hasCollided(taken, self.rect.x, self.rect.y)
+        if collided == True:
+            #print("Moving around")
+            self.rect.x = self.rect.x + r.randint(0,5)
+        elif self.rect.y + 50 > 750:
+            self.kill()
+        else:
+            #print("Moving Down")
+            self.rect.y = self.rect.y + r.randint(0,5)
     
 sprites = pg.sprite.Group()
-init_screen=main(1000,500)
+init_screen=main(1000,750)
 
-#e1=enemySprite(100,200,10,init_screen.screen)
-#e2=enemySprite(200,200,10,init_screen.screen)
-#e3=enemySprite(300,200,10,init_screen.screen)
-#e4=enemySprite(400,200,10,init_screen.screen)
-#e5=enemySprite(500,200,10,init_screen.screen)
+e1=enemySprite(100,200,10,init_screen.screen)
+e2=enemySprite(200,200,10,init_screen.screen)
+e3=enemySprite(300,200,10,init_screen.screen)
+e4=enemySprite(400,200,10,init_screen.screen)
+e5=enemySprite(500,200,10,init_screen.screen)
 e6=enemySprite(600,200,10,init_screen.screen)
-#e7=enemySprite(700,200,10,init_screen.screen)
-#e8=enemySprite(800,200,10,init_screen.screen)
-#e9=enemySprite(900,200,10,init_screen.screen)
+e7=enemySprite(700,200,10,init_screen.screen)
+e8=enemySprite(800,200,10,init_screen.screen)
+e9=enemySprite(900,200,10,init_screen.screen)
 
-#sprites.add(e1,e2,e3,e4,e5,e6,e7,e8,e9)
-sprites.add(e6)
+sprites.add(e1,e2,e3,e4,e5,e6,e7,e8,e9)
 
 for item in sprites:
     print(item.rect.x)
@@ -81,17 +100,23 @@ pg.display.update()
 print(sprites.sprites())
 
 while True:
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_DOWN:
+    #for event in pg.event.get():
+        #if event.type == pg.KEYDOWN:
+            #if event.key == pg.K_DOWN:
                 #screen.fill("Black")
-                for item in sprites:
-                    item.moveRandomDown(taken_coords)
-                    print("Again!")
+                #for item in sprites:
+                    #item.moveRandomDown(taken_coords)
+                    #print("Again!")
                 #print(item.rect.y)
-                screen.fill("Black")
-                print("DOWN")
+                #screen.fill("Black")
+                #print("DOWN")
 
+    for item in sprites:               
+        item.moveRandomDown(taken_coords)
+    screen.fill("Black")
+    init_screen.initialise(points,level,bank,name)
     init_screen.generateBlocks()
     sprites.draw(screen)
     pg.display.update()
+    clock = pg.time.Clock()
+    clock.tick(20)
