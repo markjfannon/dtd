623019ca0f47bd6b02930b1a1526035ddeeac53f
markjfannon <markjfannon@github.com>
2022-03-06 16:05:12 +0000

    Added random movement, rectangles. Need to sort out collision avoidance.....

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import pygame as pg
 import random as r
 
 screen = pg.display.set_mode((1000,500))
+taken_coords = ["200:250,300:400","500:800,300:400"]
+
 
 class Rectangle():
     def __init__(self, x, y, l, h, screen):
@@ -17,8 +19,10 @@ class main:
         self.screen = pg.display.set_mode((windowX, windowY))
         pg.display.set_caption("Discipline Tower Defence")
         self.screen.fill("Black")
-        rect1=Rectangle(200,300,50,100,self.screen)
         pg.display.flip()
+    def generateBlocks(self):
+        rect1=Rectangle(200,300,50,100,self.screen)
+        rect2=Rectangle(500,300,300,100,self.screen)
 
 
 class enemySprite(pg.sprite.Sprite):
@@ -31,32 +35,42 @@ class enemySprite(pg.sprite.Sprite):
         self.rect.y = y
         self.speed = v
         #screen.blit(self.image, (x,y))
-    def moveDown(self):
-        self.rect.y = self.rect.y + self.speed
-        if self.rect.y > 450:
-            print("No longer possible")
-            self.kill()
-    def moveRandomDown(self):
-        self.rect.y = self.rect.y + r.randint(0,10)
-        if self.rect.y > 450:
-            print("No longer possible")
-            self.kill()
-        
+    def moveRandomDown(self,taken):
+        l=len(taken)
+        t=0
+        collided = 1
+        while t < l:
+            split=taken[t].split(",")
+            x=split[0]
+            y=split[1]
+            x=x.split(":")
+            y=y.split(":")
+            #if int(collided+1) == int(1):
+               # break
+            if self.rect.x  >= int(x[0]) and self.rect.x <= int(x[1]) and self.rect.y + 50 >= int(y[0]) and self.rect.y <= int(y[1]):
+                print("Moving around")
+                self.rect.x = self.rect.x + 17
+                t=t+10000000000
+            else:
+                print("Moving Down")
+                self.rect.y = self.rect.y + r.randint(0,10)
+            t=t+1
     
 sprites = pg.sprite.Group()
 init_screen=main(1000,500)
 
-e1=enemySprite(100,200,10,init_screen.screen)
-e2=enemySprite(200,200,10,init_screen.screen)
-e3=enemySprite(300,200,10,init_screen.screen)
-e4=enemySprite(400,200,10,init_screen.screen)
-e5=enemySprite(500,200,10,init_screen.screen)
+#e1=enemySprite(100,200,10,init_screen.screen)
+#e2=enemySprite(200,200,10,init_screen.screen)
+#e3=enemySprite(300,200,10,init_screen.screen)
+#e4=enemySprite(400,200,10,init_screen.screen)
+#e5=enemySprite(500,200,10,init_screen.screen)
 e6=enemySprite(600,200,10,init_screen.screen)
-e7=enemySprite(700,200,10,init_screen.screen)
-e8=enemySprite(800,200,10,init_screen.screen)
-e9=enemySprite(900,200,10,init_screen.screen)
+#e7=enemySprite(700,200,10,init_screen.screen)
+#e8=enemySprite(800,200,10,init_screen.screen)
+#e9=enemySprite(900,200,10,init_screen.screen)
 
-sprites.add(e1,e2,e3,e4,e5,e6,e7,e8,e9)
+#sprites.add(e1,e2,e3,e4,e5,e6,e7,e8,e9)
+sprites.add(e6)
 
 for item in sprites:
     print(item.rect.x)
@@ -72,10 +86,12 @@ while True:
             if event.key == pg.K_DOWN:
                 #screen.fill("Black")
                 for item in sprites:
-                    item.moveRandomDown()
+                    item.moveRandomDown(taken_coords)
+                    print("Again!")
                 #print(item.rect.y)
                 screen.fill("Black")
                 print("DOWN")
 
+    init_screen.generateBlocks()
     sprites.draw(screen)
     pg.display.update()
