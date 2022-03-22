f696a0b6a10932a32e641b5d9c2770a3731f0458
markjfannon <41549138+markjfannon@users.noreply.github.com>
2022-03-22 09:23:52 +0000

    Added shop

--- game.py
+++ game.py
@@ -55,8 +55,6 @@ def hasHit(taken,bullet_x,bullet_y):
     else:
         return False, 0, 0
 
-            
-
 class Rectangle():
     def __init__(self, x, y, l, h, screen, colour):
         self.x = x
@@ -65,13 +63,48 @@ class Rectangle():
         self.h = h
         pg.draw.rect(screen, colour, pg.Rect(self.x, self.y, self.l, self.h))
 
+class Shop():
+    def __init__(self, x, y, screen):
+        shopfont=pg.font.SysFont("sans-serif",30)
+        bodyfont=pg.font.SysFont("sans-serif",25)
+        exitCross = pg.image.load("cross.png")
+        exitCross = pg.transform.scale(exitCross, (50, 50))
+        self.x = x
+        self.y = y
+        self.l = 600
+        self.h = 450
+        self.screen = screen
+        outline = [Rectangle(self.x, self.y, self.l, self.h, self.screen, "Black"),
+                   Rectangle(self.x+2, self.y+2, self.l-4, self.h-4, self.screen, "Grey")]
+        self.screen.blit(exitCross,(self.x+self.l-50, self.y))
+        lines = [Rectangle(self.x,self.y+50,self.l,2, self.screen, "Black"),
+        Rectangle(self.x,self.y+185,self.l,2, self.screen, "Black"),
+        Rectangle(self.x,self.y+315,self.l,2, self.screen, "Black")]
+        item1 = [Rectangle(self.x+20, self.y+70, 100, 100, self.screen, "Black"),Rectangle(self.x+22, self.y+72, 96, 96, self.screen, "Brown")]
+        item2 = [Rectangle(self.x+20, self.y+200, 100, 100, self.screen, "Black"),Rectangle(self.x+22, self.y+202, 96, 96, self.screen, "Yellow")]
+        item3 = [Rectangle(self.x+20, self.y+335, 100, 100, self.screen, "Black"),Rectangle(self.x+22, self.y+337, 96, 96, self.screen, "Blue")]
+        self.screen.blit(shopfont.render(("Bronze Turret"),False,"Black"),(self.x+145,self.y+75))
+        self.screen.blit(bodyfont.render(("This turret does a small amount of damage"),False,"Black"),(self.x+145,self.y+100))
+        
+        
+        
+        
+        
+
+    def update (self,position):
+        if position[0] in range(self.x+self.l-50,self.x+self.l) and position[1] in range(self.y, self.y + 50):
+            global shop, running
+            shop=False
+            running=True
+
 class Turret(Rectangle):
-    def __init__(self,x,y,size,screen,colour):
+    def __init__(self,x,y,size,screen,colour, v):
         self.x = x
         self.y = y
         self.size=size
         self.screen = screen
         self.bullets = pg.sprite.Group()
+        self.v = v
         super().__init__(x, y, size, size, screen, "Black")
         pg.draw.rect(screen, colour, pg.Rect(self.x+3, self.y+3, self.l-6, self.h-6))
         pg.draw.rect(screen,"Black", pg.Rect(self.x+size/2 - 6, self.y+4, 12 ,size/2))
@@ -81,9 +114,9 @@ class Turret(Rectangle):
         global enemy_coords
         for item in enemy_coords:
             enemy=item.split(",")
-            print(enemy)
+            #print(enemy)
             if int(enemy[0]) > self.x and int(enemy[0]) < self.x+self.size  and int(enemy[1]) > 200:
-                print((item.split(","))[0])
+                #print((item.split(","))[0])
                 los=True
                 break
         global bullets
@@ -92,18 +125,23 @@ class Turret(Rectangle):
         elif los == False:
             pass
         else:
-            bullets.add(Bullet(self.screen,r.randint(self.x,self.x+90),self.y))
-            print("generated")
+            n=r.randint(1,8)
+            if n > 2:
+                pass
+            else:
+                bullets.add(Bullet(self.screen,r.randint(self.x,self.x+90),self.y,self.v))
+            #print("generated")
 
 
 class Bullet(pg.sprite.Sprite):
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, v):
         super().__init__()
         #self.image = pg.draw.rect(screen, "Orange", pg.Rect(x, y, 7, 10))
         self.image = pg.transform.scale((pg.image.load("bullet.png")),(10,30))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.speed = v
 
     def move(self, coords):
         if self.rect.y <= 40:
@@ -115,7 +153,7 @@ class Bullet(pg.sprite.Sprite):
             if c == True:
                 print("Hit")
                 self.kill()
-            self.rect.y=self.rect.y - 5
+            self.rect.y=self.rect.y - self.speed
 
 class main:
     def __init__(self, windowX, windowY):
@@ -127,31 +165,34 @@ class main:
 
     def initialise(self,points,level,bank,name,health):
         font=pg.font.SysFont("sans-serif",60)
+        shopfont=pg.font.SysFont("sans-serif",50)
         topBar=Rectangle(0,0,1000,40,self.screen,"Grey")
         self.screen.blit(logoIcon,(0,0))
         self.screen.blit(font.render(str(points),False,"Black"),(160,2))
         self.screen.blit(font.render(("Level "+str(level)),False,"Black"),(310,2))
         self.screen.blit(font.render(("$"+str(bank)),False,"Black"),(600,2))
         self.screen.blit(font.render(name,False,"Black"),(750,2))
+        shopButton=Rectangle(910,0,90,40,self.screen,"Black")
+        shopButtonInner=Rectangle(912,2,86,36,self.screen,"Green")
+        self.screen.blit(shopfont.render("Shop",False,"Black"),(912,2))
         progBarOutline=Rectangle(60,55,904,28,self.screen,"Black")
         progBar=Rectangle(62,57,health,24,self.screen, "Green")
         
     def generateBlocks(self):
         #rect1=Rectangle(200,300,50,100,self.screen,"Blue")
         #rect2=Rectangle(500,300,300,100,self.screen,"Blue")
-        self.turrets = [Turret(50,450,100,self.screen,"Red"),Turret(850,450,100,self.screen,"Red"),Turret(450,300,100,self.screen,"Red")]
+        self.turrets = [Turret(50,450,100,self.screen,"Red",4),Turret(850,450,100,self.screen,"Red",2),Turret(450,300,100,self.screen,"Red",5)]
         
         
 
 class enemySprite(pg.sprite.Sprite):
-    def __init__(self, x, y, v, screen):
+    def __init__(self, x, y, screen):
         super().__init__()
         self.image = pg.transform.scale((pg.image.load("creature1.png")),(50,50))
         #pg.draw.rect(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = v
         self.hit = False 
         #screen.blit(self.image, (x,y))
 
@@ -165,31 +206,22 @@ class enemySprite(pg.sprite.Sprite):
             health=health-20
             self.kill()
         else:
-            #print("Moving Down")
-            self.rect.y = self.rect.y + r.randint(0,5)
+            if r.randint(1,10) == 7:
+                self.rect.x = self.rect.x + r.randint(0,5)
+            else:
+                self.rect.y = self.rect.y + r.randint(0,5)
+
+start=input("Start?")
 
 sprites = pg.sprite.Group()
 bullets = pg.sprite.Group()
 init_screen=main(1000,600)
+shop1=Shop(20,35,init_screen.screen)
 init_screen.generateBlocks()
-
-e1=enemySprite(100,200,10,init_screen.screen)
-e2=enemySprite(200,200,10,init_screen.screen)
-e3=enemySprite(300,300,10,init_screen.screen)
-e4=enemySprite(400,200,10,init_screen.screen)
-e5=enemySprite(500,200,10,init_screen.screen)
-e6=enemySprite(600,200,10,init_screen.screen)
-e7=enemySprite(700,200,10,init_screen.screen)
-e8=enemySprite(800,200,10,init_screen.screen)
-e9=enemySprite(900,200,10,init_screen.screen)
-
-b1=Bullet(init_screen.screen,311,400)
-b2=Bullet(init_screen.screen,400,400)
-
-sprites.add(e1,e2,e3,e4,e5,e6,e7,e8,e9)
 
 for item in init_screen.turrets:
     item.shoot()
+
 
 screen.fill("White")
 sprites.draw(screen)
@@ -197,35 +229,48 @@ bullets.draw(screen)
 pg.display.update()
 print(sprites.sprites())
 
-while True:
-    #for event in pg.event.get():
-        #if event.type == pg.KEYDOWN:
-            #if event.key == pg.K_DOWN:
-                #screen.fill("Black")
-                #for item in sprites:
-                    #item.moveRandomDown(taken_coords)
-                    #print("Again!")
-                #print(item.rect.y)
-                #screen.fill("Black")
-                #print("DOWN")
+shop=False
+running=True
 
+while True:
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+        if event.type == pg.MOUSEBUTTONDOWN:
+            pos=pg.mouse.get_pos()
+            if pos[0] >= 910 and pos[1] >= 0 and pos[1] <= 40:
+                print("shop!")
+                shop=True
+                running=False
+            shop1.update(pos)
+    
+    if len(sprites) > 10:
+        pass
+    else:
+        sprites.add(enemySprite(r.randint(1,900),100,init_screen.screen))
     enemy_coords= []
-    for item in sprites:               
-        item.moveRandomDown(taken_coords)
-        enemy_coords.append(str(item.rect.x)+","+str(item.rect.y))
-    for item in init_screen.turrets:
-        item.shoot()
-    for item in bullets:
-        item.move(enemy_coords)
-    for item in sprites:
-        if item.hit == True:
-            item.kill()
+
+    if running == True:
+        for item in sprites:               
+            item.moveRandomDown(taken_coords)
+            enemy_coords.append(str(item.rect.x)+","+str(item.rect.y))
+        for item in init_screen.turrets:
+            item.shoot()
+        for item in bullets:
+            item.move(enemy_coords)
+        for item in sprites:
+            if item.hit == True:
+                item.kill()
     screen.fill("White")
     init_screen.initialise(points,level,bank,name,health)
     init_screen.generateBlocks()
     #bullets.add(init_screen.turret1.bullet1)
     sprites.draw(screen)
     bullets.draw(screen)
+    if shop == True:
+        shop1=Shop(20,35,init_screen.screen)
+        pg.display.flip()
     pg.display.update()
     clock = pg.time.Clock()
     clock.tick(20)
